fecdd2a6752fcaaecad68ebf7ef164ba06fc71a5
Henry Doan <henrydoan96@gmail.com>
2021-06-22 19:59:06 -0600

    provider logic done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import Home from './components/shared/Home';
+import Dash from './components/shared/Dash';
+import Nomatch from './components/shared/Nomatch';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Navbar from './components/shared/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Navbar />
+    <Container>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/dash' component={Dash} />
+        <Route component={Nomatch} />
+      </Switch>
+    </Container>
+  </>
+)
 
 export default App;
